6bb8be5d9ddcec4429843aba6da4ab089695f4ba
finnmans <57464711+finnmans@users.noreply.github.com>
2020-10-07 01:19:15 -0300

    Adicionando a classe tabuleiro
    Classe tabuleiro, ate agora só desenha o tabuleiro, porem já tem o espaço reservado para chamar a jogada.

---
+++ src/com/lpoo/Tabuleiro.java
@@ -0,0 +1,37 @@
+package src.com.lpoo;
+
+public class Tabuleiro {
+   //private char[][] gridTabuleiro = new char[12][28];
+    private Integer [][] jogarTabuleiro = new Integer[6][7];
+   
+    public void desenharTabuleiro(){
+        
+        System.out.println(" (0) (1) (2) (3) (4) (5) (6)");
+         
+        for(int i = 0;i < 13;i++){ 
+        
+            for(int j = 0;j<29;j++){
+
+                if(j==0 || j==28) System.out.printf("|"); //print das laterais
+               
+                if(i%2!=0){ //condição de entrada nas partes sem grid
+                    if(j%4==0 && (j>0 && j<28)) System.out.printf("|"); //print das divisorias da grid dentro do range 1 a 27 e não printar 2 coisas no msm espaço
+                    
+                    else if(j%2!=0 && (j>0 && j<28)) System.out.printf(" "); //print para organizar
+                   
+                    else if(j>0 && j<28) System.out.printf("0"); //print que vai ser substituido pelo numero do jogador
+                }
+               
+                else{ //condição de entrada para grid                   
+                    if(j%4==0 && (j>0 && j<28) ) System.out.printf("."); //divisoria de 4 dentro do range 1 a 27 e não printar 2 coisas no msm espaço
+                    
+                    else if(i!=12 && (j>0 && j<28))System.out.print("-");
+                    
+                    else if(j>0 && j<28)System.out.print("=");
+                }     
+        
+            }
+            System.out.print("\n");
+        } 
+    }
+}
