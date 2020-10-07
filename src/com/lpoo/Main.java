2a1942f35345a258704c98320876943e02ce621b
finnmans <57464711+finnmans@users.noreply.github.com>
2020-10-07 11:17:33 -0300

    Iniciou o tabuleiro na main

--- src/com/lpoo/Main.java
+++ src/com/lpoo/Main.java
@@ -12,10 +12,14 @@ public class Main {
 
   public static void main(String[] args) {
     Partida partida = new Partida("Alex", "Felipe");
+    Tabuleiro tabuleiro = new Tabuleiro();
+
 
     partida.FazerJogada(0);
     partida.FazerJogada(1);
     partida.FazerJogada(2);
     partida.FazerJogada(3);
+
+    tabuleiro.desenharTabuleiro();
   }
 }
