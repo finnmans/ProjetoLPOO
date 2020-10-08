4539d43386ac768ca1a8ce0c7cf0946561a50b94
Alex Candido <alexandresouzape@gmail.com>
2020-10-08 10:16:09 -0300

    Tabuleiro Fix: * Renomeei peçaTabuleiro por matrizTabuleiro  o "Ç" na variável pode dar problema
    * Tirei aquele if(linhaAtual == 0) continue;
    ele era redundante
    
    * Tirei os Getters e Setters, eram inúteis também.
    Eu que tinha entendido errado a estrutura das coisas.
    
    * Coloquei um \n no final de desenhar Tabuleiro
    só pra ficar mais legível no console.

--- src/com/lpoo/Tabuleiro.java
+++ src/com/lpoo/Tabuleiro.java
@@ -1,59 +1,47 @@
 package src.com.lpoo;
 
 public class Tabuleiro {
-    private int[][] peçaTabuleiro = new int[6][7]; //peças no tabuleiro
+  private int[][] matrizTabuleiro = new int[6][7]; // peças no tabuleiro
 
-    public int[][] getPeçaTabuleiro() {
-        return peçaTabuleiro;
-    }
+  public void desenharTabuleiro() { // desenhar o tabuleiro
 
-    public void setPeçaTabuleiro(int[][] peçaTabuleiro) {
-        this.peçaTabuleiro = peçaTabuleiro;
-    }
+    System.out.println(" (0) (1) (2) (3) (4) (5) (6)");// casas possiveis
+    System.out.println("|---.---.---.---.---.---.---|");// linhas de separação
 
-    public void desenharTabuleiro() { //desenhar o tabuleiro
-
-        System.out.println(" (0) (1) (2) (3) (4) (5) (6)");
-        System.out.println("|---.---.---.---.---.---.---|");
-
-        for (int i = 0; i < 6; i++) {
-            System.out.print("|");
-            for (int j = 0; j < 7; j++) {
-
-                if (peçaTabuleiro[i][j] == 0) //condição para saber se eh um espaço vazio
-                    System.out.print("   |");
-                else
-                    System.out.print(" " + peçaTabuleiro[i][j] + " |");//printar onde fez a jogada
-
-            }
-            System.out.println("\n|---.---.---.---.---.---.---|");//linhas de separação
-
-        }
+    for (int i = 0; i < 6; i++) {
+      System.out.print("|");
+      for (int j = 0; j < 7; j++) {
+        String valorLinha = matrizTabuleiro[i][j] == 0 ? " " : Integer.toString(matrizTabuleiro[i][j], 10);
+        System.out.print(" " + valorLinha + " |");
+      }
+      System.out.println("\n|---.---.---.---.---.---.---|");// linhas de separação
 
     }
 
-    // Não sei se isso funciona mas parece algo utilizavel
-    public void fazerJogada(int position, int jogador) {
+    System.out.println();
 
-        if (position >= peçaTabuleiro.length) {
-            System.out.println("Jogada Invalida"); // sera alterado
-            return;
-        }
+  }
 
-        for (int i = 0; i < peçaTabuleiro.length; i++) {
-            int linhaAtual = peçaTabuleiro[i][position];
+  public void fazerJogada(int position, int jogador) {
 
-            if (linhaAtual == 0) // caso for 0, passa pra proxima
-                continue;
-            else {
-                peçaTabuleiro[i - 1][position] = jogador; // caso tenha achado alguma campo preenchido, se colocar na
-                                                          // posição anterior
-                return;
-            }
-        }
-
-        peçaTabuleiro[peçaTabuleiro.length - 1][position] = jogador; // caso n tenha achado nada, se coloca no final
-                                                                     // da coluna
+    if (position >= matrizTabuleiro.length) {
+      System.out.println("Jogada Invalida"); // sera alterado
+      return;
     }
+
+    for (int i = 0; i < matrizTabuleiro.length; i++) {
+      int linhaAtual = matrizTabuleiro[i][position];
+
+      // caso tenha achado alguma campo preenchido, se colocar na posição anterior
+      if (linhaAtual != 0) {
+        matrizTabuleiro[i - 1][position] = jogador;
+        return;
+      }
+    }
+
+    // caso n tenha achado nada, se coloca no final da coluna
+    matrizTabuleiro[matrizTabuleiro.length - 1][position] = jogador;
+
+  }
 
 }
