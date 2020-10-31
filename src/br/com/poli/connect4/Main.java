7a772deb3a5b8820bb0c327898655a6a56485e2a
Alex Candido <alexandresouzape@gmail.com>
2020-10-30 21:20:31 -0300

    Refator na Main, com checagem de vitória agora

--- src/br/com/poli/connect4/Main.java
+++ src/br/com/poli/connect4/Main.java
@@ -13,37 +13,43 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    String jog1, jog2;
-
-    Scanner scn1 = new Scanner(System.in);
-    Scanner scn2 = new Scanner(System.in);
-    Scanner play = new Scanner(System.in);
+    String jog1;
+    String jog2;
+    Scanner scanner = new Scanner(System.in);
+    Jogador vencedor = null;
+    boolean empate = false;
+    // String msgBaseJogada = "%-20s: jogou na coluna: %x %n";
 
     System.out.println("Jogador 1:");
-    jog1 = scn1.next();
+    jog1 = scanner.next();
 
     System.out.println("Jogador 2:");
-    jog2 = scn2.next();
+    jog2 = scanner.next();
 
     Partida partida = new Partida(jog1, jog2);
 
-    System.out.printf("%n%-20s: começará jogando!%n", partida.getJoadorAtual());
+    System.out.printf("%n%-20s: começará jogando!%n", partida.getJogadorAtual());
     System.out.println(String.format("%-40s%n", " ").replace(" ", "-")); // Printar a linha no tamanho certo
+    System.out.println(partida.getTabuleiroString());
 
-    // String msgBase = "%-20s: jogou na coluna: %x %n";
-    do {// loop eterno da partida
+    while (!empate && vencedor == null) {
+      System.out.printf("%s | Digite a posição (0-6): ", partida.getJogadorAtual());
+      int x = scanner.nextInt();
+      System.out.println(partida.fazerJogada(x));
 
-      System.out.println(partida.fazerJogada(play.nextInt()));
-      // win = checkWin(); //verifica se o jogador ganhou a partida
-      if (partida.isEmpate()) {
-        System.out.println("ITS A TIE!");
-        break;
-      }
+      vencedor = partida.getVencedor();
+      empate = partida.isEmpate();
+    }
 
-    } while (true);
-    System.out.println("Jogo acabou"); // ta aq so pra saber se tem erro em algum lugar
-    play.close();
-    scn1.close();
-    scn2.close();
+    if (vencedor != null) {
+      System.out.printf("%n %s VENCEU!!%n", vencedor);
+    }
+
+    if (empate) {
+      System.out.println("ITS A TIE!");
+    }
+
+    System.out.println("\nJogo acabou"); // ta aq so pra saber se tem erro em algum lugar
+    scanner.close();
   }
 }
