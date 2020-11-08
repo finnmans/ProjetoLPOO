9a0c3aee9183480de1268daefaeb40dbe81ffa34
Alex Candido <alexandresouzape@gmail.com>
2020-11-08 15:59:13 -0300

    Comentários na Exception

--- src/br/com/poli/exceptions/FullColumnException.java
+++ src/br/com/poli/exceptions/FullColumnException.java
@@ -2,12 +2,11 @@ package br.com.poli.exceptions;
 
 /**
  * FullColumnException
+ * 
+ * Exceção caso a coluna do tabuleiro já esteja completamente preenchida
  */
 public class FullColumnException extends Exception {
 
-  /**
-   *
-   */
   private static final long serialVersionUID = 1L;
 
   public FullColumnException(String message, Throwable cause) {
