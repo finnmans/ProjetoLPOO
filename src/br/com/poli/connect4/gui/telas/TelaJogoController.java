f31f8be5ba3e00e6e54a39a2ba90e25531ae193a
Alex Candido <alexandresouzape@gmail.com>
2020-11-10 15:46:13 -0300

    Fonte Funcionando

--- src/br/com/poli/connect4/gui/telas/TelaJogoController.java
+++ src/br/com/poli/connect4/gui/telas/TelaJogoController.java
@@ -10,7 +10,10 @@ import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextFlow;
 import javafx.scene.text.Text;
-import java.awt.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,14 +33,15 @@ public class TelaJogoController implements Initializable {
     this.partida = builder.MakePartida();
     
     System.out.println("[TelaJogo] Inicializando");
-    System.out.println("[TelaJogo] Jogadores:");
-    System.out.println("[TelaJogo] " + builder.getUserName(0));
-    System.out.println("[TelaJogo] " +builder.getUserName(1));
-    
-//    Font font = Font.loadFont("file:gui/fonts/Blambot Casual Bold.ttf",32);
-//    Font.loadFont(JanelaPrincipal.class.getResource("fonts/Blambot Casual Bold.ttf").toExternalForm(), 10);
-//    j1.setFont(font);
-//    j2.setFont(font);
+    System.out.println("[TelaJogo] " +  builder.getUserName(0));
+    System.out.println("[TelaJogo] " +  builder.getUserName(1));
+    System.out.println("\n \t[TelaJogo>Partida] \n" +  partida.getTabuleiroString());
+
+    Font font = Font.loadFont(JanelaPrincipal.class.getResource("fonts/Blambot-Casual-Bold.ttf").toExternalForm(),32);
+//    Font font = Font.loadFont(JanelaPrincipal.class.getResource("fonts/Blambot Casual Bold.ttf").toExternalForm(), 10);
+    System.out.println(font);
+    j1.setFont(font);
+    j2.setFont(font);
     j1.setText(builder.getUserName(0));
     j2.setText(builder.getUserName(1));
 
